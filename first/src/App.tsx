067d2dd33f3718b8279bb3a476e71526334297aa
afank14 <103207126+afank14@users.noreply.github.com>
2024-03-04 14:52:25 -0700

    finish the mission 9 videos

--- first/src/App.tsx
+++ first/src/App.tsx
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Define what the Band Properties are going to look like
+interface BandProps {
+  name: string;
+  members: string;
+  formed: number;
+}
+
+const bandNames = [
+  {
+    name: 'The Beastie Boys',
+    members: 'Ad Rock, MCA, Mike D',
+    formed: 1979,
+  },
+  {
+    name: 'Wolfmother',
+    members: 'Andrew Stockdale, Hamish Rosser, Bobby Poulton',
+    formed: 2004,
+  },
+  {
+    name: 'Cream',
+    members: 'Eric Clapton, Jack Bruce, Ginger Baker',
+    formed: 1966,
+  },
+  {
+    name: 'Nirvana',
+    members: 'Kurt Cobain, Dave Groul, Krist Novoselic',
+    formed: 1987,
+  },
+];
+
+function Welcome() {
+  return (
+    <div>
+      <h1>The Best Music Trios Ever</h1>
+      <h3>According to Professor Hilton</h3>
+    </div>
+  );
+}
+
+class Band extends React.Component<BandProps> {
+  render() {
+    const oneBand = this.props;
+    return (
+      <div>
+        <img />
+        <h2>{oneBand.name}</h2>
+        <h3>Members: {oneBand.members}</h3>
+        <h3>Formed: {oneBand.formed}</h3>
+      </div>
+    );
+  }
+}
+
+function BandList() {
+  return (
+    <div>
+      {/* <Band {...bandNames[0]} />
+      <Band {...bandNames[1]} />
+      <Band {...bandNames[2]} /> */}
+
+      {bandNames.map((oneBand) => (
+        <Band {...oneBand} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
